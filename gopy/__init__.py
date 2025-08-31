82013cf03d1b9793c11d58bd5433e8688c1066fd
Anton Vice <a1996nton@gmail.com>
2025-08-31 13:58:38 -0400

    chore: Release v0.1.3

--- gopy/__init__.py
+++ gopy/__init__.py
@@ -6,7 +6,7 @@ inspired by the concurrency model of the Go language.
 """
 
 __author__ = "anton"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .app import Channel, GoroutineManager, go, nc
 
