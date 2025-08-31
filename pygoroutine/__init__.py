e1723ce5ffe8757ac12babe926dbc09e8fb855f3
Anton Vice <a1996nton@gmail.com>
2025-08-31 14:22:40 -0400

    chore: Release v0.1.5

--- pygoroutine/__init__.py
+++ pygoroutine/__init__.py
@@ -6,7 +6,7 @@ inspired by the concurrency model of the Go language.
 """
 
 __author__ = "Anton Vice"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .app import Channel, GoroutineManager, go, nc
 
