3081422cf85dbff5f364bdb72ade50d972548dd6
Anton Vice <a1996nton@gmail.com>
2025-08-31 14:18:12 -0400

    asd

--- pygoroutine/__init__.py
+++ pygoroutine/__init__.py
@@ -5,8 +5,8 @@ This library provides a simple, powerful API for concurrent programming,
 inspired by the concurrency model of the Go language.
 """
 
-__author__ = "anton"
-__version__ = "0.1.4"
+__author__ = "Amtom Vice"
+__version__ = "0.1.5"
 
 from .app import Channel, GoroutineManager, go, nc
 
